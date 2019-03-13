1651cdbb74dc5a61ca49794093a12e175aadfde1
adChong <bdgczma@nus.edu.sg>
2019-03-13 08:42:55 +0800

    implementation of UtilIdf abstract class using Eppy

--- src/idf_functions.py
+++ src/idf_functions.py
@@ -1,4 +1,11 @@
 from abc import abstractmethod
+from eppy.modeleditor import IDF
+
+def equal(a, b):
+    try:
+        return a.upper() == b.upper()
+    except AttributeError:
+        return a == b
 
 class UtilIdf():
 
@@ -25,10 +32,46 @@ class UtilIdf():
 
 
     @abstractmethod
-    def get_output(self, arg):
+    def get_output(self):
         """
 
-        :param arg: name of argument to extract model output
         :return: model output of dtype=float
         """
         pass
+
+
+class EppyUtilIdf(UtilIdf):
+
+    def copy(self):
+        idf_txt = self.idf.idfstr()
+        try:
+            idf_copy = IDF(epw=getattr(self.idf, 'epw'))
+        except AttributeError:
+            idf_copy = IDF()
+        idf_copy.initreadtxt(idf_txt)
+        return idf_copy
+
+    def mod(self, obj_id, obj_name, field, value):
+        idf = self.copy(self.idf)
+        obj_lst = idf.idfobjects[obj_id]
+        for obj in obj_lst:
+            if obj_name == obj.Name or obj_name == 'ALL':
+                obj[field] = value
+        return idf
+
+    def get_output(self, html_tbls, arg_lst):
+        if not arg_lst:
+            return None
+        elif len(arg_lst) == 1:
+            key = arg_lst.pop(0)
+            for tbl in html_tbls:
+                heading = tbl[0]
+                if equal(heading, key):
+                    return tbl[1:]
+        else:
+            key = arg_lst.pop(0)
+            for tbl in html_tbls:
+                heading = tbl[0]
+                if equal(heading, key):
+                    return self.get_output(tbl[1], arg_lst)
+        return None
